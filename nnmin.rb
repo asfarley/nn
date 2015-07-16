6c45046c321b49a515de4936da87a76bb15eef43
Alexander Farley <alexander.s.farley@gmail.com>
2015-07-16 19:42:40 -0400

    eliminate a few more lines

--- nnmin.rb
+++ nnmin.rb
@@ -3,24 +3,19 @@ class NN
     @weights = Array.new(output_size).collect!{ Array.new(input_size+1).collect!{ |x| rand}}
     @L = learning_rate
   end
-  def train(input, target)
-	update_weights(weight_gradients(input, evaluate(input), target))
-  end
   def evaluate(input)
     output = Array.new(@weights.length)
     for i in 0..(@weights.length-1)
-	  input_biased = input.clone.insert(0,1) #Clone because insert is destructive
-	  neuron_input = @weights[i].zip(input_biased).inject(0) { |result, element| result + element[0]*element[1] }
+	  neuron_input = @weights[i].zip(input.clone.insert(0,1)).inject(0) { |result, element| result + element[0]*element[1] }
       output[i] = 1/(1+2.71**-neuron_input)
     end
     return output
   end
   def weight_gradients(input, output, target)
 	e = output.map.with_index{ |x,i| output[i] - target[i] }
-    input_biased = input.clone.insert(0,1) #Clone because insert is destructive
     weight_gradients = Array.new(@weights.length)
     for i in 0..(@weights.length-1)
-      weight_gradients[i] = Array.new(@weights[i].length).collect!.with_index { |x,j| input_biased[j] * e[i] }
+      weight_gradients[i] = Array.new(@weights[i].length).collect!.with_index { |x,j| input.clone.insert(0,1)[j] * e[i] }
     end
     return weight_gradients
   end
@@ -28,7 +23,7 @@ class NN
 	 @weights.each_with_index{ |x,i| x.collect!.with_index {|x,j| x - @L*weight_gradient[i][j] } } 
   end
   def train_on_set(inputs, targets, epochs)
-  epochs.times { |i| train(inputs[i%inputs.length], targets[i%inputs.length]) }
+  epochs.times{|i| update_weights(weight_gradients(inputs[i%inputs.length], evaluate(inputs[i%inputs.length]), targets[i%inputs.length])) }
   end
 end
 
